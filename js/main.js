7e5dcc5be096e6e9ecafbc208a5c1601c6f05b99
Chris Goosen <chris@cgoosen.com>
2019-04-16 19:42:52 -0500

    JS Updates

--- js/main.js
+++ js/main.js
@@ -25,6 +25,22 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
+  $('a.about-button').click(function (e) {
+      if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+      currentWidth = $('.panel-cover').width()
+      if (currentWidth < 960) {
+        $('.panel-cover').addClass('panel-cover--collapsed')
+        $('.content-wrapper').addClass('animated slideInRight')
+      } else {
+        $('.panel-cover').css('max-width', currentWidth)
+        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      }
+    })
+
+  if (window.location.hash && window.location.hash == '#about') {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+    }
+
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
